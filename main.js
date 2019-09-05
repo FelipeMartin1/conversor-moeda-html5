bfa86eed09f1fcd93ceec609dc75753aeee306d6
Felipe Martin <martin-felipe@hotmail.com>
2019-09-05 10:18:16 +0000

    refatoracao: remocao de funcoes globais

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-function chamarAPI(){
-	console.log("Chamando API");
-	const endpoint = "api/latest";
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint,oParametros ,callback); 
-}
+const ovly = { };
 
-function callback(resultado){
-//	console.log(resultado);
+ovly.conversor = {
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint,oParametros ,this.callback); 
+	},
+	callback: function(resultado){
+		//	console.log(resultado);
 	
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
 	
-	$("#valor_destino").text(fValor);
-	$("#moeda_destino_celula").text(sMoedaDestino);	
-} 
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);	
+	}
+}
